c17a1cf975867b3927b9f0d9dab2a3fc493aca82
Apoorv yash <apoorvyash123@gmail.com>
2023-06-26 01:48:13 +0530

    Create Find_middle_of_LL.cpp

---
+++ 17.06.2023_5/Find_middle_of_LL.cpp
@@ -0,0 +1,47 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node *findMiddle(Node *head) {
+
+    Node* dbl=head;
+    Node* sgl=head;
+    if(head==NULL){
+        return head;
+    }
+    
+    while(dbl->next && dbl->next->next){
+        
+        dbl=dbl->next->next;
+        sgl=sgl->next;
+    }
+    if(dbl->next==NULL){
+            return sgl;
+        }
+        else{
+            return sgl->next;
+        }
+}
+
